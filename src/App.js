296ed54d002b7690c72e932775c1f19c547cd125
nitin <nkmrt30032003@gmail.com>
2023-11-07 23:48:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+
+import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import AppScreen from './component/AppScreen.js'; 
+
+function App() {
+  return (
+    <div className='bg-dark container-fluid'>
+      <AppScreen /> {}
+    </div>
+  );
+}
+
+export default App;
+
